88daa1035156a96406a1c04c444ca5cfd0a6b8fc
Sahil Madan <Hillatio@gmail.com>
2021-08-06 10:23:17 +1000

    Add more movement shortcuts

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -4,3 +4,7 @@ vim.g.mapleader = ','
 -- Map keys to jump to beginning/end of line
 vim.api.nvim_set_keymap('n', 'H', '^', {noremap = true})
 vim.api.nvim_set_keymap('n', 'L', '$', {noremap = true})
+vim.api.nvim_set_keymap('n', 'dH', 'd^', {noremap = true})
+vim.api.nvim_set_keymap('n', 'dL', 'd$', {noremap = true})
+vim.api.nvim_set_keymap('n', 'cH', 'c^', {noremap = true})
+vim.api.nvim_set_keymap('n', 'cL', 'c$', {noremap = true})
