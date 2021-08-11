b4e9f01c8527a49a247f928cbc6533526ae498ef
Sahil Madan <Hillatio@gmail.com>
2021-08-11 10:17:56 +1000

    Update configs

--- .config/nvim/lua/plugin/telescope.lua
+++ .config/nvim/lua/plugin/telescope.lua
@@ -6,3 +6,5 @@ vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { nor
 vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>Telescope git_status<CR>', { noremap = true})
 -- List function names and variables from Treesitter
 vim.api.nvim_set_keymap('n', '<leader>fr', '<cmd>Telescope treesitter<CR>', { noremap = true})
+-- List registers
+vim.api.nvim_set_keymap('n', '<leader>fp', '<cmd>Telescope registers<CR>', { noremap = true})
